e4a48a5af85a0aba12b4e40a24a5e1be566f6005
Niel Kenneth <niels@reign.com.au>
2022-11-26 11:54:16 +0800

    cart context update

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -11,6 +11,13 @@ const CartContext = createContext({
   add: () => {}
 });
 
+const totalAmount = (lineItems) => {
+  return lineItems.reduce((prevAmount, item) => {
+    const price = item.price.sale_price || item.price.original_price;
+    return prevAmount + (price * item.quantity)
+  }, 0);
+}
+
 const cartReducer = (state, action) => {
   switch(action.type) {
     case 'ADD':
@@ -19,22 +26,34 @@ const cartReducer = (state, action) => {
       if ( state.lineItems.length) {
         const itemIndex = state.lineItems.findIndex(lineItem => lineItem.id === product.id);
         if ( itemIndex !== -1 ) {
-          console.log('updated');
           updatedItems = [...state.lineItems];
-          console.log(updatedItems[itemIndex].quantity);
           updatedItems[itemIndex].quantity++;
-          console.log(updatedItems[itemIndex].quantity);
         } else {
           updatedItems = [...state.lineItems, product];
         }
       } else {
         updatedItems = [...state.lineItems, product];
       }
-      console.log(updatedItems)
-      // const updatedAmount = updatedItems.reduce((prevAmount, item) => prevAmount + (item.price * item.quantity), 0);
+      const updatedAmount = totalAmount(updatedItems)
       return {
         lineItems: updatedItems,
-        totalAmount: 0
+        totalAmount: updatedAmount
+      };
+      break;
+    case 'REMOVE':
+      const productID = action.payload.id;
+      const filteredItems = state.lineItems.filter(lineItem => lineItem.id !== productID);
+      if ( filteredItems.length) {
+        const updatedAmount = totalAmount(filteredItems);
+        return {
+          lineItem: filteredItems,
+          totalAmount: updatedAmount
+        }
+      } else {
+        return {
+          lineItems: [],
+          totalAmount: 0
+        }
       }
     default: return state;
   }
@@ -43,39 +62,22 @@ const cartReducer = (state, action) => {
 export const CartContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, cartState);
 
-  // const [state, setCartState] = useState(cartState);
-
   const handleAddToCart = useCallback( (product) => {
-    // let updatedItems;
-    //   if ( state.lineItems.length) {
-    //     const itemIndex = state.lineItems.findIndex(lineItem => lineItem.id === product.id);
-    //     if ( itemIndex !== -1 ) {
-    //       updatedItems = [...state.lineItems];
-    //       updatedItems[itemIndex].quantity += 1;
-    //     } else {
-    //       updatedItems = state.lineItems.push(product);
-    //     }
-    //   } else {
-    //     updatedItems = state.lineItems.concat(product);
-    //   }
-    //   console.log(updatedItems);
-    // setCartState(prevState => {
-    //   return {
-    //     ...prevState,
-    //     lineItems: updatedItems
-    //   }
-    // })
     dispatch({ type: 'ADD', payload: product });
   });
 
-  // const cartContext = {
-  //   lineItems: state.lineItem,
-  //   totalAmount: state.totalAmount,
-  //   add: handleAddToCart
-  // }
+  const handleRemoveItem = (id) => {
+    dispatch({ type: 'REMOVE', payload: { id } });
+  }
+
+  const cartContext = {
+    lineItems: state.lineItem,
+    totalAmount: state.totalAmount,
+    add: handleAddToCart
+  }
 
   return (
-    <CartContext.Provider value={{...state, add: handleAddToCart }}>
+    <CartContext.Provider value={{...cartContext}}>
       {children}
     </CartContext.Provider>
   )
